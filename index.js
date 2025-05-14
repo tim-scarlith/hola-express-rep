48b232d46a3eb29835d47db06130f8fa2a22ac9a
tim-scarlith <53355647+tim-scarlith@users.noreply.github.com>
2025-05-14 17:14:36 -0600

    Update index.js
    Fedora w Dep

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>¡Hola Mundo desde Express en NodeJS (otra vez desde Fedora)!</h1>');
+  res.send('<h1>¡Hola Mundo desde Express en NodeJS (otra vez desde Fedora Works)!</h1>');
 });
 
 app.listen(port, () => {
