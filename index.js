efa338bdb5104de9ed6919b50a8434d833487afe
tim-scarlith <anthony00052023@gmail.com>
2025-05-14 16:15:57 -0600

    Actualización

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>¡Hola Mundo desde Express en NodeJS (otra vez desde Fedora)!</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
